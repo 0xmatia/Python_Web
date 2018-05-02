66727ad3c52e4f2698c75bc54a7cbc9c97c39ef4
Elad Matia <elad.matia@gmail.com>
2018-05-02 18:58:17 +0300

    GIT

--- Class4/Project/Pink_Floyd_Client.py
+++ Class4/Project/Pink_Floyd_Client.py
@@ -1,13 +1,44 @@
 import socket
 
+connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # make the socket global
+
 
 def main():
     IP = "127.0.0.1"
     PORT = 9011
-    connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    command_list = ["Alist", "SongsInAlb", "SongLen", "GetLyc", "WhichAlb", "SByName", "SByLyc", "Exit"]
     address = (IP, PORT)
     connection.connect(address)
     print(connection.recv(1024).decode())  # connection to server was successful
+    choice = "Alist"
+    while not choice == command_list[7]:
+        print(" >>>>>>Command list: Alist, SongsInAlb, SongLen, GetLyc, WhichAlb, SByName, SByLyc, Exit<<<<<<")
+        choice = input("Enter command: ")
+        if choice == command_list[0]:
+            assemble_request(command_list[0])
+        elif choice == command_list[1]:
+            album_name = input("Enter album name: ")
+            assemble_request(command_list[1], album_name)
+        elif choice == command_list[2]:
+            song_name = input("Enter song name: ")
+            assemble_request(command_list[2], song_name)
+        elif choice == command_list[3]:
+            song_name = input("Enter song name: ")
+            assemble_request(command_list[3], song_name)
+        elif choice == command_list[4]:
+            song_name = input("Enter song name: ")
+            assemble_request(command_list[4], song_name)
+        elif choice == command_list[5]:
+            word = input("Enter a word: ")
+            assemble_request(command_list[5], word)
+        elif choice == command_list[6]:
+            word = input("Enter a word: ")
+            assemble_request(command_list[6], word)
+
+
+def assemble_request(command, parameter=""):
+    #  print(("command=" + command + "&parameter=" + parameter).encode())
+    connection.sendall(("command=" + command + "&parameter=" + parameter).encode())
 
 
 if __name__ == '__main__':
--- Class4/Project/Pink_Floyd_Server.py
+++ Class4/Project/Pink_Floyd_Server.py
@@ -3,15 +3,58 @@ import socket
 
 def main():
     PORT = 9011
+    print("Listening on port 9011")
+    call_function = {"Alist": Alist, "SongsInAlb": SongsInAlb, "SongLen": SongLen, "GetLyc": GetLyc, "SByName": SByName,
+                     "SByLyc": SByLyc}
     listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_address = ('', PORT)
     listen_socket.bind(server_address)
-    while True:
-        listen_socket.listen(1)
-        client_socket, client_address = listen_socket.accept()
+    listen_socket.listen(1)
+    client_socket, client_address = listen_socket.accept()
+    command = ""
+    while not command == "EXIT":
+        print("Loop")
         client_socket.sendall("Welcome to Pink Floyd database!".encode())
+        response = client_socket.recv(1024).decode()
+        command, parameter = disassemble_response(response)
+        call_function[command](parameter)
+    client_socket.close()
+    listen_socket.close()
+
+
+def disassemble_response(client_response):
+    command = client_response[8:client_response.index('&')]
+    parameter = client_response[client_response.index('&') + 11:]
+    return command, parameter
+
+
+def Alist(parameter):
+    print("Alist function activated")
+
+
+def SongsInAlb(paramter):
+    print("SongsInAlb function activated")
+
+
+def SongLen(parameter):
+    print("SongsLen function activated")
+
+
+def GetLyc(parameter):
+    print("GetLyc function activated")
+
+
+def WhichAlb(parameter):
+    print("WhichAlb function activated")
+
+
+def SByName(parameter):
+    print("SByName function activated")
+
+
+def SByLyc(parameter):
+    print("SByLyc function activated")
 
 
 if __name__ == '__main__':
     main()
-
