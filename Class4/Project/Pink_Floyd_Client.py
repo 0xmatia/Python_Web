1bef4fb493a0488c6fadf5351bfd764202c12f2d
Elad Matia <elad.matia@gmail.com>
2018-05-04 08:40:08 +0300

    eXCEPTIONS

--- Class4/Project/Pink_Floyd_Client.py
+++ Class4/Project/Pink_Floyd_Client.py
@@ -6,39 +6,55 @@ connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # make the socke
 def main():
     IP = "127.0.0.1"
     PORT = 9011
+    flag = True
     command_list = ["Alist", "SongsInAlb", "SongLen", "GetLyc", "WhichAlb", "SByName", "SByLyc", "Exit"]
     address = (IP, PORT)
     connection.connect(address)
     print(connection.recv(1024).decode())  # connection to server was successful
     choice = "Alist"
-    while not choice == command_list[7]:
+    while not choice == command_list[7] and flag:
         print(" >>>>>>Command list: Alist, SongsInAlb, SongLen, GetLyc, WhichAlb, SByName, SByLyc, Exit<<<<<<")
         choice = input("Enter command: ")
         if choice == command_list[0]:
-            assemble_request(command_list[0])
+            flag = assemble_request(command_list[0])
         elif choice == command_list[1]:
             album_name = input("Enter album name: ")
-            assemble_request(command_list[1], album_name)
+            flag = assemble_request(command_list[1], album_name)
         elif choice == command_list[2]:
             song_name = input("Enter song name: ")
-            assemble_request(command_list[2], song_name)
+            flag = assemble_request(command_list[2], song_name)
         elif choice == command_list[3]:
             song_name = input("Enter song name: ")
-            assemble_request(command_list[3], song_name)
+            flag = assemble_request(command_list[3], song_name)
         elif choice == command_list[4]:
             song_name = input("Enter song name: ")
-            assemble_request(command_list[4], song_name)
+            flag = assemble_request(command_list[4], song_name)
         elif choice == command_list[5]:
             word = input("Enter a word: ")
-            assemble_request(command_list[5], word)
+            flag = assemble_request(command_list[5], word)
         elif choice == command_list[6]:
             word = input("Enter a word: ")
-            assemble_request(command_list[6], word)
+            flag = assemble_request(command_list[6], word)
+    #  send exit request
+    assemble_request(command_list[7])
 
 
 def assemble_request(command, parameter=""):
-    #  print(("command=" + command + "&parameter=" + parameter).encode())
-    connection.sendall(("command=" + command + "&parameter=" + parameter).encode())
+    """
+    The function tries to send request to server and throws exception if failes
+    :param command: The command the user asked
+    :type command: str
+    :param parameter: the parameter required, empty by default
+    :type parameter: str
+    :return:False if the program failed to contact server
+    :rtype: bool
+    """
+    try:
+        connection.sendall(("command=" + command + "&parameter=" + parameter).encode())
+        return True
+    except Exception:
+        print("Can't reach server. Program is being terminated.")
+        return False
 
 
 if __name__ == '__main__':
--- Class4/Project/Pink_Floyd_Server.py
+++ Class4/Project/Pink_Floyd_Server.py
@@ -4,25 +4,39 @@ import socket
 def main():
     PORT = 9011
     print("Listening on port 9011")
-    call_function = {"Alist": Alist, "SongsInAlb": SongsInAlb, "SongLen": SongLen, "GetLyc": GetLyc, "SByName": SByName,
-                     "SByLyc": SByLyc}
+    call_function = {"Alist": Alist, "SongsInAlb": SongsInAlb, "SongLen": SongLen, "GetLyc": GetLyc,
+                     "WhichAlb": WhichAlb, "SByName": SByName, "SByLyc": SByLyc}
     listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_address = ('', PORT)
     listen_socket.bind(server_address)
-    listen_socket.listen(1)
-    client_socket, client_address = listen_socket.accept()
-    command = ""
-    while not command == "EXIT":
-        print("Loop")
-        client_socket.sendall("Welcome to Pink Floyd database!".encode())
-        response = client_socket.recv(1024).decode()
-        command, parameter = disassemble_response(response)
-        call_function[command](parameter)
-    client_socket.close()
+
+    while True:  # the server runs forever, never closes. the second while is for the client socket, which closes
+        listen_socket.listen(1)
+        client_socket, client_address = listen_socket.accept()
+        command = ""
+        while not command == "Exit":
+            try:  # try to send response, throw exception if you can't
+                client_socket.sendall("Welcome to Pink Floyd database!".encode())
+                response = client_socket.recv(1024).decode()
+                command, parameter = disassemble_response(response)
+                if not command == "Exit":
+                    call_function[command](parameter)
+            except Exception:
+                print("Client disconnected unexpectedly")
+                break
+        client_socket.close()
+        print("Connection closed. Listening on port 9011")
     listen_socket.close()
 
 
 def disassemble_response(client_response):
+    """
+    The function disassembles the request to command and parameter
+    :param client_response:the response of the clients
+    :type client_response: str
+    :return:command the parameter
+    :rtype: tuple
+    """
     command = client_response[8:client_response.index('&')]
     parameter = client_response[client_response.index('&') + 11:]
     return command, parameter
