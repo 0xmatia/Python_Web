161e5adc30023e3d1eb5bb59906e2f1f5842c8f7
Elad Matia <elad.matia@gmail.com>
2018-05-31 18:54:30 +0300

    Weather sniffer, one more to go

--- Class7/Magshimshark_EladMatia.py
+++ Class7/Magshimshark_EladMatia.py
@@ -8,7 +8,7 @@ def main():
     choice = 1
     while 3 >= choice >= 1:
         print("1 - DNS filter")
-        print("2 - DNS filter")
+        print("2 - Weather filter")
         print("3 - DNS filter")
         print("4 - DNS filter")
         choice = int(input())
@@ -16,7 +16,7 @@ def main():
             sniff(lfilter=filerDNS, prn=processDNS)
             print("\n\n\n")
         elif choice == 2:
-            print("FUck")
+            sniff(lfilter=filter_weather, prn=process_weather)
 
 
 def filerDNS(packet):
@@ -37,6 +37,24 @@ def processDNS(packet):
     """
     num_of_answers = packet[DNS].ancount
     print(str(packet[DNS].an[num_of_answers-1].rdata))
+
+
+def filter_weather(packet):
+    """
+    The function checks if the packets sniffed are weather packers
+    :param packet: the packet to check
+    :type packet: packet
+    :return: True if the packet is a weather packet, false otherwise
+    :rtype: bool
+    """
+    # print(packet.show())
+    return IP in packet and packet[IP].src == "52.89.157.137" and Raw in packet and "200:ANSWER" in packet[Raw].load.decode()
+
+
+def process_weather(packet):
+    info = packet[Raw].load.decode()
+    result = info[info.find("=")+1:info.find("&city")] + ": " + info[info.find("&text") + len("&text") + 1:]+", " + info[info.find("=", 40)+1:info.find("&text")] + " degrees in " + info[info.find("&city") + len("&city") + 1: info.find("&temp")]
+    print(result)
 
 
 if __name__ == '__main__':
