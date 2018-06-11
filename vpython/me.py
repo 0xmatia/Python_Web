2e6396705d7d0ccd63bf5ea99bab7b74483250bc
Elad Matia <elad.matia@gmail.com>
2018-06-11 19:41:12 +0300

    final

--- vpython/me.py
+++ vpython/me.py
@@ -2,13 +2,11 @@ from vpython import *
 
 box(pos=vector(0, 0, 0), length=10, height=0.5, width=10, color=color.blue)
 
-ball = sphere(pos=vector(0, 10, 0), radius=1, color=color.red)
-ball.velocity = vector(1, -2, 0)
+ball = sphere(pos=vector(0, 1, 0), radius=1, color=color.red)
 dt = 0.01
 while True:
+    vec = input("Enter x, y ")
+    vec = vec.split(",")
+    ball.velocity = vector(int(vec[0]), 0, int(vec[1]))
     rate(100)
     ball.pos = ball.pos + ball.velocity * dt
-    if ball.pos.y < ball.radius:
-        ball.velocity.y = abs(ball.velocity.y)
-    else:
-        ball.velocity.y = ball.velocity.y - 9.8*dt
